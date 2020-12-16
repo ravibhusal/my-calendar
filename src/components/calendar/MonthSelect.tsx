a5296683f118f5b0fc49185e58fd954de4b16f0a
Ravi <ravi.bhusal.np@gmail.com>
2020-12-16 21:52:18 +0545

    Implement YearSelect and MonthSelect

--- src/components/calendar/MonthSelect.tsx
+++ src/components/calendar/MonthSelect.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 function MonthSelect(props: any){
 
+  const months: Array<String> = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+  "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+
+  const onMonthSelect = (event: any) => {
+    let month = event.currentTarget.dataset.month;
+    props.onMonthSelect(month);
+  }
+
+  let monthList = months.map((month, index) => {
+    var classesToAdd = index + 1 == props.selectedMonth ? "bg-red-500" : "hover:bg-red-200"
+    return <td data-month= {index + 1} className={` rounded-full w-1/3 ${classesToAdd}`} onClick={ e =>{
+      onMonthSelect(e)
+    }}>{month}</td>;
+  })
+
+  const renderMonths = () => {
+    var cells: Array<any> = [];
+    var rows: Array<any> = [];
+
+    monthList.forEach((row, i) => {
+      if (i % 3 !== 0) {
+        cells.push(row);
+      } else {
+        rows.push(cells);
+        cells = [];
+        cells.push(row);
+      }
+      if (i === monthList.length - 1) {
+        rows.push(cells);
+      }
+    });
+
+    let elements: any = []
+
+    rows.map((d: any, i: any) => {
+      elements.push(<tr>{d}</tr>);
+    });
+    return elements;
+  }
+
+  return(
+    <table className="table-auto w-1/5 text-center shadow-lg">
+      <thead className="bg-red-200">
+        <th></th>
+        <th>Month</th>
+        <th></th>
+      </thead>
+      <tbody>{renderMonths()}</tbody>
+    </table>
+  )
 }
 
 export default MonthSelect;
--- src/components/calendar/YearSelect.tsx
+++ src/components/calendar/YearSelect.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 function YearSelect(props: any){
 
+  const currentYear = props.currentDate.format('YYYY');
+
+  const onYearSelect = (event: any) =>{
+    let year = event.currentTarget.dataset.year;
+    props.onYearSelect(year);
+  }
+
+  let yearList: any = [];
+  for ( var i = parseInt(currentYear) - 20; i <= parseInt(currentYear) + 20; i ++ ){
+    var classesToAdd = i === parseInt(props.selectedYear) ? 'bg-red-500' : "hover:bg-red-200"
+    yearList.push(<td key={i} data-year={i} className={` rounded-full w-1/3 ${classesToAdd}`} onClick={ e =>{
+      onYearSelect(e)
+    }}>{i}</td>)
+  }
+
+  const renderMonths = () => {
+    var cells: Array<any> = [];
+    var rows: Array<any> = [];
+
+    yearList.forEach((row: any, i: any) => {
+      if (i % 3 !== 0) {
+        cells.push(row);
+      } else {
+        rows.push(cells);
+        cells = [];
+        cells.push(row);
+      }
+      if (i === yearList.length - 1) {
+        rows.push(cells);
+      }
+    });
+
+    let elements: any = []
+
+    rows.map((d: any, i: any) => {
+      elements.push(<tr className="w-full">{d}</tr>);
+    });
+    return elements;
+  }
+
+  return(
+    <table className="table-auto max-h-60 w-1/5 text-center shadow-lg">
+      <thead className="bg-red-200">
+        <th></th>
+        <th>Year</th>
+        <th></th>
+      </thead>
+      <tbody>{renderMonths()}</tbody>
+    </table>
+  );
 }
 
 export default YearSelect;
